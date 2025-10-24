54fa7113031eab67add1939084c4f5b221805518
unknown <Dakota>
2025-10-24 14:03:10 -0400

    react template no bootstrap

---
+++ ui/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <Container>
+      <h1 className="mt-3">Replace me with your own stuff!</h1>
+    </Container>
+  );
+}
+
+export default App;
